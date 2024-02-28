bdfa3c6923f44201af84e9f911af51c5e1c6056c
Ivan Perez <ivan_perez0980@proton.me>
2024-02-27 21:43:39 -0300

    poniendo la forma mas facil de llamar a una funcion en react mediante un controlador de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,42 @@ const App = () => {
   const [value, setValue] = useState(10)
 
 
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // imprime el nuevo valor en la consola
-    setValue(newValue)
+
+  //FORMA MAS ACOTADA
+
+  // const setToValue = (newValue) => () => {
+  //   console.log('value now', newValue)  // imprime el nuevo valor en la consola
+  //   setValue(newValue)
+  // }
+
+
+  //FORMA MAS FACIL DE ENTENDER
+  
+  const setToValue = (newValue) => {
+    return () =>{
+      console.log('value now', newValue) // imprime el nuevo valor
+      setValue(newValue);
+    }
+  }
+
+  const hello = (who) => {
+    return () => {
+      console.log('hello', who)
+    }
   }
 
   return (
     <div>
       {value}
 
-      <button onClick={setToValue(value+1000)}>thousand</button>
+      <button onClick={setToValue(1000)}>thousand</button>
       <button onClick={setToValue(0)}>reset</button>
       <button onClick={setToValue(value + 1)}>increment</button>
+
+      <hr/>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('aang')}>button</button>
     </div>
   )
 }
